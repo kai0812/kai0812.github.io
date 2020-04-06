cc4fcad3a84e298acbcb5d4f72d01f9084e5172e
kai0812 <tomoyoshi04124@gmail.com>
2020-04-07 02:26:36 +0900

    mobileUI追加

--- 9min26sec/script.js
+++ 9min26sec/script.js
@@ -14,11 +14,16 @@ $('html').keyup(function (e) {
 		reset();
 	}
 });
-$('.reset').click(function () {
+
+$('.reset').on('click touchstart', function () {
 	reset();
 });
-$('.button').click(function () {
-	inputNumber($(this).data('num'));
+$('.button').on('click touchstart', function () {
+	if (0 < $(this).data('num')) {
+		inputNumber($(this).data('num'));
+	}else {
+		reset();
+	}
 });
 
 function inputNumber(input) {
@@ -30,7 +35,7 @@ function inputNumber(input) {
 	}
 	if (dif_cnt == 9) {
 		num[num_cnt] = input;
-		$('.button' + input).css('opacity', 0);
+		$('.button' + input).css('visibility', 'hidden');
 		$('.n' + num_cnt).text(input);
 		num_cnt++;
 	}
@@ -39,6 +44,7 @@ function inputNumber(input) {
 		displayCircle();
 	}
 }
+
 function displayCircle() {
 	var total = 0;
 	var total2 = 0;
@@ -58,27 +64,46 @@ function displayCircle() {
 			$('.s' + (-i + 9)).css('background-color', 'black');
 		}
 	}
-	
-	
+
 	$('.circle').css('opacity', 1);
 	if (flag_cnt == 18) {
 		$('.clear').addClass('displayClear');
 		$('.clear-img').css('display', 'inline-block');
-		
-	}else {
+		displayShareButton();
+		$(window).resize(function () {
+			displayShareButton();
+		});
+	} else {
 		flag_cnt = 0;
 	}
-	
 }
 function reset() {
 	var reset_num = [1, 2, 3, 4, 5, 7, 8];
 	for (var i = 1; i < 8; i++) {
 		num[i] = 0;
 		$('.mid-num').text('?');
-		$('.button' + reset_num[i - 1]).css('opacity', 1);
+		$('.button' + reset_num[i - 1]).css('visibility', 'visible');
 	}
 	num_cnt = 1;
 	flag = false;
 	$('.circle').css('opacity', 0);
 	flag_cnt = 0;
 }
+
+function displayShareButton() {
+	var window_width = window.innerWidth;
+	var window_height = window.innerHeight;
+	var clear_width = $('.clear-img').width();
+	var clear_height = $('.clear-img').height();
+	$('.share-btn').css({
+		top: window_height / 2 + clear_height / 2 + 'px',
+		right: window_width / 2 - clear_width / 2 + 'px',
+	});
+}
+
+var windowWidth = window.innerWidth;
+var windowHeight = window.innerHeight;
+$('table').css('top', (windowHeight / 100) * 15 + 'px');
+$('.button-wrap').css({
+	top: (windowHeight / 100) * 20 + (windowWidth / 100) * 24 + 'px',
+});
